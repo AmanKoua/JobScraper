27f3e278bd7120d835483bb7773344e2f459bde1
Aman <akoua@umich.edu>
2023-11-29 16:37:49 -0500

    indeed URL generation functional

--- indeed.js
+++ indeed.js
@@ -1,6 +1,7 @@
 /*
 
-   "Detroit, MI" will become "Detroit%2C%20MI"
+    "full stack developer" will become "full+stack+developer"
+   "Detroit, MI" will become "Detroit%2C+MI"
 
 
 */
@@ -9,10 +10,26 @@ let getIndeedJobs = async (baseIndeedURL, position, location, minMatchCount, key
     console.log("---------------");
     // console.log(baseIndeedURL, position, location, minMatchCount, keys);
 
-    let encodedLocation = encodeURI(location);
+    let commaIdx = undefined;
+    let encodedLocation = location;
+    let encodedPosition = position;
+    let encodedURL = undefined;
 
-    console.log(encodedLocation);
+    commaIdx = encodedLocation.indexOf(",");
 
+    if (commaIdx != -1) {
+        // encodedLocation = encodedLocation.substring(0, commaIdx + 1) + " " + encodedLocation.substring(commaIdx + 1, encodedLocation.length);
+    }
+    else {
+        console.error("Indeed location does not include a comma. Invalid location!");
+        return;
+    }
+
+    encodedLocation = encodedLocation.replaceAll(",", "%2C+");
+    encodedPosition = encodedPosition.replaceAll("-", "+");
+    encodedURL = `${baseIndeedURL}?q=${encodedPosition}&l=${encodedLocation}`;
+
+    console.log(encodedURL);
 }
 
 exports.getIndeedJobs = getIndeedJobs;
